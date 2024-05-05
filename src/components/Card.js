9d43c13b1183cd779d8a8757cd58fafba795b16f
kubigor <kubigor.ik@gmail.com>
2024-05-04 17:33:22 -0700

    fixed text shift

--- src/components/Card.js
+++ src/components/Card.js
@@ -16,10 +16,10 @@ const Card = ({ index }) => {
 
   return <div id="card-container">
       <a id="card-link" href={data[index].link} target="_blank"><div id="card-header">
-        <img loading="lazy" id="source-logo" src={logoSource}/>
-        <img loading="lazy" id="card-avatar" src={data[index].avatar}/>
+        <img loading="lazy" id="source-logo" src={logoSource} alt="Source of review (Yelp or Google)"/>
+        <img loading="lazy" id="card-avatar" src={data[index].avatar} alt="Customer avatar"/>
         <h2 id="card-name">{data[index].name}</h2>
-        <img loading="lazy" id="five-star" src={stars}/>
+        <img loading="lazy" id="five-star" src={stars} alt="Five stars"/>
       </div>
       </a>
       <p id="card-message">{data[index].message}</p>
